2ed0bc25adee7c0a4b1cd72e4192c54a7592a977
Nitish <nhurduke@ptc.com>
2025-12-19 10:07:24 +0530

    Change Detection - avoid change dection using NgZone runOutsideAngular function

--- change-detection/src/app/counter/counter.component.ts
+++ change-detection/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -9,7 +9,27 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+  zone = inject(NgZone);
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    setTimeout(() => {
+      console.log('Timer Expired!!');
+      // Even though this has nothing to do with other components, Change dectection
+      // mechanism works on all the components by zone.js
+    }, 5000);
+
+    //To avoid this wrap this function around runOutsideAngular function of NgZone object
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Second Timer Expired!!');
+        //This wont be watched by zone.js and change detection will not work on this.
+      }, 5000);
+    });
+  }
   count = signal(0);
 
   get debugOutput() {
