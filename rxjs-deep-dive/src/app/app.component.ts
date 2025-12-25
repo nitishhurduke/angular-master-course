7963df9a55eeeef177e1e345c1d70e8177ab450b
Nitish <nhurduke@ptc.com>
2025-12-25 19:17:01 +0530

    Rxjs | Basic Observable operation

--- rxjs-deep-dive/src/app/app.component.ts
+++ rxjs-deep-dive/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: (value) => console.log(value),
+      complete: () => console.log('Operation completed successfully!'),
+      error: (err) => console.log('Something went wrong, ', err.message),
+    });
 
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
