ac10e40dee9d9d23ff974e6f81296d5e7cfaa1c3
Nitish <nhurduke@ptc.com>
2025-12-25 19:50:23 +0530

    Rxjs | Converting Signal to Observable

--- rxjs-deep-dive/src/app/app.component.ts
+++ rxjs-deep-dive/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,17 +16,35 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
-  ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(map((value) => value * 2))
-      .subscribe({
-        next: (value) => console.log(value),
-        complete: () => console.log('Operation completed successfully!'),
-        error: (err) => console.log('Something went wrong, ', err.message),
-      });
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
 
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
+  constructor() {
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount()} times.`);
+    // });
+  }
+
+  ngOnInit(): void {
+    // const subscription = interval(1000)
+    //   .pipe(map((value) => value * 2))
+    //   .subscribe({
+    //     next: (value) => console.log(value),
+    //     complete: () => console.log('Operation completed successfully!'),
+    //     error: (err) => console.log('Something went wrong, ', err.message),
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+
+    const subscription = this.clickCount$.subscribe({
+      next: (value) =>
+        console.log(`Clicked button ${this.clickCount()} times.`),
     });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+
+  onClick() {
+    this.clickCount.update((prevValue) => prevValue + 1);
   }
 }
