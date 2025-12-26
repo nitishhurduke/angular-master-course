04fa149425e5a600ed47979ddfe5ba476e9168cc
Nitish <nhurduke@ptc.com>
2025-12-26 13:09:24 +0530

    Http | Handle Error usecase while fetching data from backend

--- http-deep-dive/src/app/places/available-places/available-places.component.ts
+++ http-deep-dive/src/app/places/available-places/available-places.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -14,6 +14,7 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent implements OnInit {
   isFetching = signal(false);
+  error = signal('');
   places = signal<Place[] | undefined>(undefined);
   httpClient = inject(HttpClient);
   private backendUrl = 'http://localhost:3000';
@@ -21,10 +22,25 @@ export class AvailablePlacesComponent implements OnInit {
     this.isFetching.set(true);
     this.httpClient
       .get<{ places: Place[] }>(this.backendUrl + '/places')
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () => new Error('Something went wrong, Please try again later.')
+          );
+        })
+      )
       .subscribe({
-        next: (places) => this.places.set(places),
-        complete: () => this.isFetching.set(false),
+        next: (places) => {
+          this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
       });
   }
 }
