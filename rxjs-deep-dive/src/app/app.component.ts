2d3c027049e29d5f08b87b0f9f18e46747887ec3
Nitish <nhurduke@ptc.com>
2025-12-26 11:08:08 +0530

    Rxjs | Custom Observable using new Observable class

--- rxjs-deep-dive/src/app/app.component.ts
+++ rxjs-deep-dive/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {
   signal,
 } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +20,17 @@ export class AppComponent implements OnInit {
   clickCount$ = toObservable(this.clickCount);
   interval$ = interval(1000);
   intervalSignal = toSignal(this.interval$, { initialValue: 0 });
-
+  customInterval$ = new Observable((subscriber) => {
+    let custCount = 0;
+    const inervalFunc = setInterval(() => {
+      custCount = custCount + 1;
+      if (custCount > 3) {
+        clearInterval(inervalFunc); // Important to handle cleaning steps before complete to avoid memory leaks
+        subscriber.complete();
+      }
+      subscriber.next(custCount);
+    }, 2000);
+  });
   constructor() {
     // effect(() => {
     //   console.log(`Clicked button ${this.clickCount()} times.`);
@@ -38,12 +48,18 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // });
-
-    const subscription = this.clickCount$.subscribe({
-      next: (value) =>
-        console.log(`Clicked button ${this.clickCount()} times.`),
+    // const subscription = this.clickCount$.subscribe({
+    //   next: (value) =>
+    //     console.log(`Clicked button ${this.clickCount()} times.`),
+    // });
+    // this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    const custSubscription = this.customInterval$.subscribe({
+      next: (value) => console.log(`Custom count : ${value}`),
+      complete: () => {
+        console.log('Custom count has finished');
+        this.destroyRef.onDestroy(() => custSubscription.unsubscribe());
+      },
     });
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
 
   onClick() {
