39cc0326afb9b27da0a511daeef2a273c5fd9e24
Nitish <nhurduke@ptc.com>
2026-01-05 20:00:52 +0530

    Forms | Reactive form basic functions

--- forms-deep-dive/src/app/auth/login/login.component.ts
+++ forms-deep-dive/src/app/auth/login/login.component.ts
@@ -1,54 +1,32 @@
-import { afterNextRender, Component, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
 
-  constructor() {
-    afterNextRender(() => {
-      const savedFormData = window.localStorage.getItem('saved-login-form');
-      if (savedFormData) {
-        const loadedFormData = JSON.parse(savedFormData);
-        const savedEmail = loadedFormData.email;
-        // this.form().setValue({
-        //   email: savedFormData,
-        //   password: '',
-        // });
-
-        // Throws error as it won't get controls initially.
-        // Easy workaround, wait for 1 ms
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-      }
-      this.form()
-        .valueChanges?.pipe(debounceTime(1000))
-        .subscribe({
-          next: (value) => {
-            window.localStorage.setItem(
-              'saved-login-form',
-              JSON.stringify({
-                email: value.email,
-              })
-            );
-          },
-        });
-    });
-  }
-
-  onSubmit(formData: NgForm) {
-    console.log(formData.form);
-    console.log(formData.value.email);
-    console.log(formData.value.password);
-
-    formData.form.reset();
+  onSubmit() {
+    console.log(this.form);
+    const emailValue = this.form.controls.email.value;
+    const passwordValue = this.form.value.password;
+    console.log(emailValue, passwordValue);
   }
 }
