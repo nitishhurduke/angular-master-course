248ae242f4c0b0a20511b77a9de970e67dd09c64
Nitish <nhurduke@ptc.com>
2026-01-05 19:26:27 +0530

    Forms | Template Driven froms advanced features
    - Use `valueChanges` function of form
    - Use control functions
    - Use `debounceTime` pipe function to get next value from sbuscription once values stops coming for the specified time.

--- forms-deep-dive/src/app/auth/login/login.component.ts
+++ forms-deep-dive/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,45 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule],
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+
+  constructor() {
+    afterNextRender(() => {
+      const savedFormData = window.localStorage.getItem('saved-login-form');
+      if (savedFormData) {
+        const loadedFormData = JSON.parse(savedFormData);
+        const savedEmail = loadedFormData.email;
+        // this.form().setValue({
+        //   email: savedFormData,
+        //   password: '',
+        // });
+
+        // Throws error as it won't get controls initially.
+        // Easy workaround, wait for 1 ms
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+      this.form()
+        .valueChanges?.pipe(debounceTime(1000))
+        .subscribe({
+          next: (value) => {
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({
+                email: value.email,
+              })
+            );
+          },
+        });
+    });
+  }
+
   onSubmit(formData: NgForm) {
     console.log(formData.form);
     console.log(formData.value.email);
     console.log(formData.value.password);
+
+    formData.form.reset();
   }
 }
