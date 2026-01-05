26518ae25b1ece3fc14e1f8c5940bd6b05ca62c6
Nitish <nhurduke@ptc.com>
2026-01-05 18:47:41 +0530

    Forms | Template driven forms basics
    - Use of ngForm to access advanced forms capabilities though angular funtions
    - Use of ngModel directive to control inputs
    - Use validations and updates template to enhance user experience accordingly

--- forms-deep-dive/src/app/auth/login/login.component.ts
+++ forms-deep-dive/src/app/auth/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    console.log(formData.form);
+    console.log(formData.value.email);
+    console.log(formData.value.password);
+  }
+}
