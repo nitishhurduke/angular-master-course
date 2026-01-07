93dedfbcd35abf809992e451d78a56ae45322419
Nitish <nhurduke@ptc.com>
2026-01-07 11:44:54 +0530

    Routing | Add routing configuration in main.ts

--- routing-deep-dive/src/main.ts
+++ routing-deep-dive/src/main.ts
@@ -1,5 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { TasksComponent } from './app/tasks/tasks.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      {
+        path: 'tasks', //<your-domain>/tasks
+        component: TasksComponent,
+      },
+    ]),
+  ],
+}).catch((err) => console.error(err));
