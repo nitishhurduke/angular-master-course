e3fc2035ce19df74e7d6dc8e05ff2fee0692436d
Nitish <nhurduke@ptc.com>
2026-01-07 19:48:40 +0530

    Routing | Extract Dynamic route parameter using traditional approach

--- routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
+++ routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,14 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,11 +16,27 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  userId = input.required<string>();
+export class UserTasksComponent implements OnInit {
+  // userId = input.required<string>();
+  userName: string = '';
   private usersService = inject(UsersService);
+  private desrtoyRef = inject(DestroyRef);
+  private activatedRoute = inject(ActivatedRoute);
+  // userName = computed(
+  //   () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  // );
 
-  userName = computed(
-    () => this.usersService.users.find((u) => u.id === this.userId())?.name
-  );
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        const userId = paramMap.get('userId');
+        this.userName =
+          this.usersService.users.find((u) => u.id === userId)?.name || '';
+      },
+    });
+
+    this.desrtoyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
