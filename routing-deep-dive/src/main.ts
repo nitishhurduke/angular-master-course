83cea01caeead1196304dda9b42b5c6720c8c815
Nitish <nhurduke@ptc.com>
2026-01-07 15:21:09 +0530

    Routing | Refactor App configuration

--- routing-deep-dive/src/main.ts
+++ routing-deep-dive/src/main.ts
@@ -1,16 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { TasksComponent } from './app/tasks/tasks.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter([
-      {
-        path: 'tasks', //<your-domain>/tasks
-        component: TasksComponent,
-      },
-    ]),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
