97b8e5cc8d7f330c7ecca00a08c4b2c04d25b5bb
Nitish <nhurduke@ptc.com>
2026-01-08 11:03:39 +0530

    Routing | Add chlidren routes

--- routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
+++ routing-deep-dive/src/app/users/user-tasks/user-tasks.component.ts
@@ -8,13 +8,14 @@ import {
 } from '@angular/core';
 
 import { UsersService } from '../users.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
 export class UserTasksComponent implements OnInit {
   // userId = input.required<string>();
